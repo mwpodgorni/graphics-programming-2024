df4a57da8629e952f386f7d55b2a46d61ffb5f91
mwpodgorni <mwpodgorni@gmail.com>
2024-02-10 13:10:47 +0100

    exercise 1.1-1.3

--- exercises/exercise01/TerrainApplication.h
+++ exercises/exercise01/TerrainApplication.h
@@ -1,33 +1,36 @@
 #pragma once
 
 #include <ituGL/application/Application.h>
-
+#include <ituGL/geometry/VertexArrayObject.h>
+#include <ituGL/geometry/VertexBufferObject.h>
+#include <ituGL/geometry/ElementBufferObject.h>
 // (todo) 01.1: Include the libraries you need
 
 
 class TerrainApplication : public Application
 {
 public:
-    TerrainApplication();
+	TerrainApplication();
 
 protected:
-    void Initialize() override;
-    void Update() override;
-    void Render() override;
-    void Cleanup() override;
+	void Initialize() override;
+	void Update() override;
+	void Render() override;
+	void Cleanup() override;
 
 private:
-    void BuildShaders();
-    void UpdateOutputMode();
+	void BuildShaders();
+	void UpdateOutputMode();
 
 private:
-    unsigned int m_gridX, m_gridY;
+	unsigned int m_gridX, m_gridY;
 
-    unsigned int m_shaderProgram;
+	unsigned int m_shaderProgram;
 
-    // (todo) 01.1: Declare an VBO, VAO
-
-
-    // (todo) 01.5: Declare an EBO
+	// (todo) 01.1: Declare an VBO, VAO
+	VertexArrayObject vao;
+	VertexBufferObject vbo;
+	// (todo) 01.5: Declare an EBO
+	ElementBufferObject ebo;
 
 };
