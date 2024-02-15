803b66d862e5e032695de33511f180c1a5a95311
mwpodgorni <mwpodgorni@gmail.com>
2024-02-15 17:45:31 +0100

    exercise 2.2

--- exercises/exercise02/ParticlesApplication.h
+++ exercises/exercise02/ParticlesApplication.h
@@ -8,48 +8,48 @@
 class ParticlesApplication : public Application
 {
 public:
-    ParticlesApplication();
+	ParticlesApplication();
 
 protected:
-    void Initialize() override;
-    void Update() override;
-    void Render() override;
+	void Initialize() override;
+	void Update() override;
+	void Render() override;
 
 private:
-    // Initialize the VBO and VAO
-    void InitializeGeometry();
+	// Initialize the VBO and VAO
+	void InitializeGeometry();
 
-    // Load, compile and link shaders
-    void InitializeShaders();
+	// Load, compile and link shaders
+	void InitializeShaders();
 
-    // Helper function to encapsulate loading and compiling a shader
-    void LoadAndCompileShader(Shader& shader, const char* path);
+	// Helper function to encapsulate loading and compiling a shader
+	void LoadAndCompileShader(Shader& shader, const char* path);
 
-    // Emit a new particle
-    void EmitParticle(const glm::vec2& position);
+	// Emit a new particle
+	void EmitParticle(const glm::vec2& position, float size);
 
-    // Helper methods for random values
-    static float Random01();
-    static float RandomRange(float from, float to);
-    static glm::vec2 RandomDirection();
-    static Color RandomColor();
+	// Helper methods for random values
+	static float Random01();
+	static float RandomRange(float from, float to);
+	static glm::vec2 RandomDirection();
+	static Color RandomColor();
 
 private:
-    // All particles stored in a single VBO with interleaved attributes
-    VertexBufferObject m_vbo;
+	// All particles stored in a single VBO with interleaved attributes
+	VertexBufferObject m_vbo;
 
-    // VAO that represents the particle system
-    VertexArrayObject m_vao;
+	// VAO that represents the particle system
+	VertexArrayObject m_vao;
 
-    // Particles shader program
-    ShaderProgram m_shaderProgram;
+	// Particles shader program
+	ShaderProgram m_shaderProgram;
 
-    // Mouse position during this frame
-    glm::vec2 m_mousePosition;
+	// Mouse position during this frame
+	glm::vec2 m_mousePosition;
 
-    // Total number of particles created
-    unsigned int m_particleCount;
+	// Total number of particles created
+	unsigned int m_particleCount;
 
-    // Max number of particles that can exist at the same time
-    const unsigned int m_particleCapacity;
+	// Max number of particles that can exist at the same time
+	const unsigned int m_particleCapacity;
 };
--- exercises/exercise02/shaders/particles.vert
+++ exercises/exercise02/shaders/particles.vert
@@ -2,6 +2,7 @@
 
 layout (location = 0) in vec2 ParticlePosition;
 // (todo) 02.X: Add more vertex attributes
+layout (location = 1) in float size;
 
 
 // (todo) 02.5: Add Color output variable here
@@ -13,4 +14,6 @@ layout (location = 0) in vec2 ParticlePosition;
 void main()
 {
 	gl_Position = vec4(ParticlePosition, 0.0, 1.0);
+	gl_PointSize = size;
 }
+
