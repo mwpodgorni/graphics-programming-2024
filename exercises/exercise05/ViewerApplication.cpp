9aa3a59c192174290fe03417c12b6c5f65d59cc2
mwpodgorni <mwpodgorni@gmail.com>
2024-03-10 10:16:50 +0100

    5.1

--- exercises/exercise05/ViewerApplication.cpp
+++ exercises/exercise05/ViewerApplication.cpp
@@ -9,187 +9,194 @@
 #include <imgui.h>
 
 ViewerApplication::ViewerApplication()
-    : Application(1024, 1024, "Viewer demo")
-    , m_cameraPosition(0, 30, 30)
-    , m_cameraTranslationSpeed(20.0f)
-    , m_cameraRotationSpeed(0.5f)
-    , m_cameraEnabled(false)
-    , m_cameraEnablePressed(false)
-    , m_mousePosition(GetMainWindow().GetMousePosition(true))
+	: Application(1024, 1024, "Viewer demo")
+	, m_cameraPosition(0, 30, 30)
+	, m_cameraTranslationSpeed(20.0f)
+	, m_cameraRotationSpeed(0.5f)
+	, m_cameraEnabled(false)
+	, m_cameraEnablePressed(false)
+	, m_mousePosition(GetMainWindow().GetMousePosition(true))
 {
 }
 
 void ViewerApplication::Initialize()
 {
-    Application::Initialize();
+	Application::Initialize();
 
-    // Initialize DearImGUI
-    m_imGui.Initialize(GetMainWindow());
+	// Initialize DearImGUI
+	m_imGui.Initialize(GetMainWindow());
 
-    InitializeModel();
-    InitializeCamera();
-    InitializeLights();
+	InitializeModel();
+	InitializeCamera();
+	InitializeLights();
 
-    DeviceGL& device = GetDevice();
-    device.EnableFeature(GL_DEPTH_TEST);
-    device.SetVSyncEnabled(true);
+	DeviceGL& device = GetDevice();
+	device.EnableFeature(GL_DEPTH_TEST);
+	device.SetVSyncEnabled(true);
 }
 
 void ViewerApplication::Update()
 {
-    Application::Update();
+	Application::Update();
 
-    // Update camera controller
-    UpdateCamera();
+	// Update camera controller
+	UpdateCamera();
 }
 
 void ViewerApplication::Render()
 {
-    Application::Render();
+	Application::Render();
 
-    // Clear color and depth
-    GetDevice().Clear(true, Color(0.0f, 0.0f, 0.0f, 1.0f), true, 1.0f);
+	// Clear color and depth
+	GetDevice().Clear(true, Color(0.0f, 0.0f, 0.0f, 1.0f), true, 1.0f);
 
-    m_model.Draw();
+	m_model.Draw();
 }
 
 void ViewerApplication::Cleanup()
 {
-    // Cleanup DearImGUI
-    m_imGui.Cleanup();
+	// Cleanup DearImGUI
+	m_imGui.Cleanup();
 
-    Application::Cleanup();
+	Application::Cleanup();
 }
 
 void ViewerApplication::InitializeModel()
 {
-    // Load and build shader
-    Shader vertexShader = ShaderLoader::Load(Shader::VertexShader, "shaders/unlit.vert");
-    Shader fragmentShader = ShaderLoader::Load(Shader::FragmentShader, "shaders/unlit.frag");
-    std::shared_ptr<ShaderProgram> shaderProgram = std::make_shared<ShaderProgram>();
-    shaderProgram->Build(vertexShader, fragmentShader);
 
-    // Filter out uniforms that are not material properties
-    ShaderUniformCollection::NameSet filteredUniforms;
-    filteredUniforms.insert("WorldMatrix");
-    filteredUniforms.insert("ViewProjMatrix");
+	// Load and build shader
+	Shader vertexShader = ShaderLoader::Load(Shader::VertexShader, "shaders/unlit.vert");
+	Shader fragmentShader = ShaderLoader::Load(Shader::FragmentShader, "shaders/unlit.frag");
+	std::shared_ptr<ShaderProgram> shaderProgram = std::make_shared<ShaderProgram>();
+	shaderProgram->Build(vertexShader, fragmentShader);
 
-    // Create reference material
-    std::shared_ptr<Material> material = std::make_shared<Material>(shaderProgram, filteredUniforms);
-    material->SetUniformValue("Color", glm::vec4(1.0f));
+	// Filter out uniforms that are not material properties
+	ShaderUniformCollection::NameSet filteredUniforms;
+	filteredUniforms.insert("WorldMatrix");
+	filteredUniforms.insert("ViewProjMatrix");
 
-    // Setup function
-    ShaderProgram::Location worldMatrixLocation = shaderProgram->GetUniformLocation("WorldMatrix");
-    ShaderProgram::Location viewProjMatrixLocation = shaderProgram->GetUniformLocation("ViewProjMatrix");
-    material->SetShaderSetupFunction([=](ShaderProgram& shaderProgram)
-        {
-            shaderProgram.SetUniform(worldMatrixLocation, glm::scale(glm::vec3(0.1f)));
-            shaderProgram.SetUniform(viewProjMatrixLocation, m_camera.GetViewProjectionMatrix());
+	// Create reference material
+	std::shared_ptr<Material> material = std::make_shared<Material>(shaderProgram, filteredUniforms);
+	material->SetUniformValue("Color", glm::vec4(1.0f));
 
-            // (todo) 05.X: Set camera and light uniforms
+	// Setup function
+	ShaderProgram::Location worldMatrixLocation = shaderProgram->GetUniformLocation("WorldMatrix");
+	ShaderProgram::Location viewProjMatrixLocation = shaderProgram->GetUniformLocation("ViewProjMatrix");
+	material->SetShaderSetupFunction([=](ShaderProgram& shaderProgram)
+		{
+			shaderProgram.SetUniform(worldMatrixLocation, glm::scale(glm::vec3(0.1f)));
+			shaderProgram.SetUniform(viewProjMatrixLocation, m_camera.GetViewProjectionMatrix());
+
+			// (todo) 05.X: Set camera and light uniforms
 
 
-        });
+		});
 
-    // Configure loader
-    ModelLoader loader(material);
-    loader.SetMaterialAttribute(VertexAttribute::Semantic::Position, "VertexPosition");
-    loader.SetMaterialAttribute(VertexAttribute::Semantic::Normal, "VertexNormal");
-    loader.SetMaterialAttribute(VertexAttribute::Semantic::TexCoord0, "VertexTexCoord");
+	// Configure loader
+	ModelLoader loader(material);
+	loader.SetCreateMaterials(true);
+	loader.SetMaterialAttribute(VertexAttribute::Semantic::Position, "VertexPosition");
+	loader.SetMaterialAttribute(VertexAttribute::Semantic::Normal, "VertexNormal");
+	loader.SetMaterialAttribute(VertexAttribute::Semantic::TexCoord0, "VertexTexCoord");
 
-    // Load model
-    m_model = loader.Load("models/mill/Mill.obj");
+	// Load model
+	m_model = loader.Load("models/mill/Mill.obj");
+	m_model = loader.Load("models/mill/Mill.obj");
 
-    // (todo) 05.1: Load and set textures
-
+	// (todo) 05.1: Load and set textures
+	Texture2DLoader textureLoader(TextureObject::FormatRGBA, TextureObject::InternalFormatRGBA8);
+	textureLoader.SetFlipVertical(true);
+	m_model.GetMaterial(0).SetUniformValue("ColorTexture", textureLoader.LoadShared("models/mill/Ground_shadow.jpg"));
+	m_model.GetMaterial(1).SetUniformValue("ColorTexture", textureLoader.LoadShared("models/mill/Ground_color.jpg"));
+	m_model.GetMaterial(2).SetUniformValue("ColorTexture", textureLoader.LoadShared("models/mill/MillCat_color.jpg"));
 }
 
 void ViewerApplication::InitializeCamera()
 {
-    // Set view matrix, from the camera position looking to the origin
-    m_camera.SetViewMatrix(m_cameraPosition, glm::vec3(0.0f));
+	// Set view matrix, from the camera position looking to the origin
+	m_camera.SetViewMatrix(m_cameraPosition, glm::vec3(0.0f));
 
-    // Set perspective matrix
-    float aspectRatio = GetMainWindow().GetAspectRatio();
-    m_camera.SetPerspectiveProjectionMatrix(1.0f, aspectRatio, 0.1f, 1000.0f);
+	// Set perspective matrix
+	float aspectRatio = GetMainWindow().GetAspectRatio();
+	m_camera.SetPerspectiveProjectionMatrix(1.0f, aspectRatio, 0.1f, 1000.0f);
 }
 
 void ViewerApplication::InitializeLights()
 {
-    // (todo) 05.X: Initialize light variables
+	// (todo) 05.X: Initialize light variables
 
 }
 
 void ViewerApplication::RenderGUI()
 {
-    m_imGui.BeginFrame();
+	m_imGui.BeginFrame();
 
-    // (todo) 05.4: Add debug controls for light properties
+	// (todo) 05.4: Add debug controls for light properties
 
-    m_imGui.EndFrame();
+	m_imGui.EndFrame();
 }
 
 void ViewerApplication::UpdateCamera()
 {
-    Window& window = GetMainWindow();
+	Window& window = GetMainWindow();
 
-    // Update if camera is enabled (controlled by SPACE key)
-    {
-        bool enablePressed = window.IsKeyPressed(GLFW_KEY_SPACE);
-        if (enablePressed && !m_cameraEnablePressed)
-        {
-            m_cameraEnabled = !m_cameraEnabled;
+	// Update if camera is enabled (controlled by SPACE key)
+	{
+		bool enablePressed = window.IsKeyPressed(GLFW_KEY_SPACE);
+		if (enablePressed && !m_cameraEnablePressed)
+		{
+			m_cameraEnabled = !m_cameraEnabled;
 
-            window.SetMouseVisible(!m_cameraEnabled);
-            m_mousePosition = window.GetMousePosition(true);
-        }
-        m_cameraEnablePressed = enablePressed;
-    }
+			window.SetMouseVisible(!m_cameraEnabled);
+			m_mousePosition = window.GetMousePosition(true);
+		}
+		m_cameraEnablePressed = enablePressed;
+	}
 
-    if (!m_cameraEnabled)
-        return;
+	if (!m_cameraEnabled)
+		return;
 
-    glm::mat4 viewTransposedMatrix = glm::transpose(m_camera.GetViewMatrix());
-    glm::vec3 viewRight = viewTransposedMatrix[0];
-    glm::vec3 viewForward = -viewTransposedMatrix[2];
+	glm::mat4 viewTransposedMatrix = glm::transpose(m_camera.GetViewMatrix());
+	glm::vec3 viewRight = viewTransposedMatrix[0];
+	glm::vec3 viewForward = -viewTransposedMatrix[2];
 
-    // Update camera translation
-    {
-        glm::vec2 inputTranslation(0.0f);
+	// Update camera translation
+	{
+		glm::vec2 inputTranslation(0.0f);
 
-        if (window.IsKeyPressed(GLFW_KEY_A))
-            inputTranslation.x = -1.0f;
-        else if (window.IsKeyPressed(GLFW_KEY_D))
-            inputTranslation.x = 1.0f;
+		if (window.IsKeyPressed(GLFW_KEY_A))
+			inputTranslation.x = -1.0f;
+		else if (window.IsKeyPressed(GLFW_KEY_D))
+			inputTranslation.x = 1.0f;
 
-        if (window.IsKeyPressed(GLFW_KEY_W))
-            inputTranslation.y = 1.0f;
-        else if (window.IsKeyPressed(GLFW_KEY_S))
-            inputTranslation.y = -1.0f;
+		if (window.IsKeyPressed(GLFW_KEY_W))
+			inputTranslation.y = 1.0f;
+		else if (window.IsKeyPressed(GLFW_KEY_S))
+			inputTranslation.y = -1.0f;
 
-        inputTranslation *= m_cameraTranslationSpeed;
-        inputTranslation *= GetDeltaTime();
+		inputTranslation *= m_cameraTranslationSpeed;
+		inputTranslation *= GetDeltaTime();
 
-        // Double speed if SHIFT is pressed
-        if (window.IsKeyPressed(GLFW_KEY_LEFT_SHIFT))
-            inputTranslation *= 2.0f;
+		// Double speed if SHIFT is pressed
+		if (window.IsKeyPressed(GLFW_KEY_LEFT_SHIFT))
+			inputTranslation *= 2.0f;
 
-        m_cameraPosition += inputTranslation.x * viewRight + inputTranslation.y * viewForward;
-    }
+		m_cameraPosition += inputTranslation.x * viewRight + inputTranslation.y * viewForward;
+	}
 
-    // Update camera rotation
-   {
-        glm::vec2 mousePosition = window.GetMousePosition(true);
-        glm::vec2 deltaMousePosition = mousePosition - m_mousePosition;
-        m_mousePosition = mousePosition;
+	// Update camera rotation
+	{
+		glm::vec2 mousePosition = window.GetMousePosition(true);
+		glm::vec2 deltaMousePosition = mousePosition - m_mousePosition;
+		m_mousePosition = mousePosition;
 
-        glm::vec3 inputRotation(-deltaMousePosition.x, deltaMousePosition.y, 0.0f);
+		glm::vec3 inputRotation(-deltaMousePosition.x, deltaMousePosition.y, 0.0f);
 
-        inputRotation *= m_cameraRotationSpeed;
+		inputRotation *= m_cameraRotationSpeed;
 
-        viewForward = glm::rotate(inputRotation.x, glm::vec3(0,1,0)) * glm::rotate(inputRotation.y, glm::vec3(viewRight)) * glm::vec4(viewForward, 0);
-    }
+		viewForward = glm::rotate(inputRotation.x, glm::vec3(0, 1, 0)) * glm::rotate(inputRotation.y, glm::vec3(viewRight)) * glm::vec4(viewForward, 0);
+	}
 
-   // Update view matrix
-   m_camera.SetViewMatrix(m_cameraPosition, m_cameraPosition + viewForward);
+	// Update view matrix
+	m_camera.SetViewMatrix(m_cameraPosition, m_cameraPosition + viewForward);
 }
